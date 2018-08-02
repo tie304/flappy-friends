64ceaee657acc3e56f04a48f4ae73cf3a1e7440d
tyler hanson <tylerhanson921@gmail.com>
2018-08-02 16:19:13 -0500

    deployment debugging

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     def __init__(self,state):
         self.secret_key = '\x93\x85\xe04\xa1\x8fLw\x16\xaaQR\x03\xb7\x87_<\x1c\xfe|Cw\x819'
         if state == "production":
-            self.database_URI == "mongodb://tyler:1youngsavage@ds127443.mlab.com:27443/flappy_friends_dev"
+            self.database_URI = "mongodb://tyler:1youngsavage@ds127443.mlab.com:27443/flappy_friends_dev"
         else:
             self.database_URI = 'mongodb://127.0.0.1:27017'
         self.database_name = 'flappy_friends'
